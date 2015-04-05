1e9646914df19b4fff087232edcf5f7b1e36383c
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-05 14:25:15 -0400

    Updating the login screen, modularizing all of the functions

--- js/index.js
+++ js/index.js
@@ -2,16 +2,39 @@
  * @author
  */
 
+//functions
+var clearOutInputs = function(){
+		//removing content from the inital input fields
+		$('#usernameInputField').val('');
+		$('#passwordInputField').val('');
+	
+	};
+	
+var toggleUsernameAndRegistrationButtonText = function(){
+		//removing content from the inital input fields
+		$('#usernameInputField').val('');
+		$('#passwordInputField').val('');
+	
+	};
+	
+
+
 $( document ).ready(function() {
 	
 	//Variables
 	var toggleHeight = true;
+	var userIsRegistering = false;
 	
 	//Setting up Parse APPID + Javascript Key
 	Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
     
 	//listening for LOGIN button to be pressed
 	$("#loginButtonPress").click(function(){
+		
+	if(userIsRegistering)
+	{
+		
+	}
    	
 	var username = $('#usernameInputField').val();
 	var password = $('#passwordInputField').val();
@@ -36,35 +59,56 @@ $( document ).ready(function() {
 
 		if(toggleHeight)
 		{
-		$(".loginBox").animate({height: '410px'});
+		$(".loginBox").animate({height: '460px'});
 		//$(".inputContainer").animate({height: '190px'});
 		$(".registrationButton").animate({ 
-       	 top: "+=90px",
+       	 top: "+=140px",
       	}, '400');
 
-		//setting an element equal to the password form field
-		$('.inputContainer').append('<input type="password" value="" class=".loginBox input center-block" placeholder="Confirm Password" id="passwordConfirmationOne" />');
+		//Rendering registration field
+		$('.inputContainer').append('<input type="password" value="" class=".loginBox input center-block" placeholder="Confirm Password" id="passwordConfirmation" />');
 		
-		$('#passwordConfirmationOne').hide().show('slow');
+		$('#passwordConfirmation').hide().show('slow');
 		
-		$('.inputContainer').append('<input type="password" value="" class=".loginBox input center-block" placeholder="Re-Enter Password" id="passwordConfirmationTwo" />');
+		$('.inputContainer').append('<input type="email" value="" class=".loginBox input center-block" placeholder="Enter Email Address" id="emailConfirmationOne" />');
 		
-		$('#passwordConfirmationTwo').hide().show('slow');
+		$('#emailConfirmationOne').hide().show('slow');
 		
+		$('.inputContainer').append('<input type="email" value="" class=".loginBox input center-block" placeholder="Confirm Email Address" id="emailConfirmationTwo" />');
+		
+		$('#emailConfirmationTwo').hide().show('slow');
+		
+		//Change the button Names
+		$("#registrationButtonPress").text('Cancel');
+		$("#loginButtonPress").text('Register');
+		
+		//Toggling Boolean values
 		toggleHeight = false;
+		userIsRegistering = true;
+		
 		
 		}//end - if statement
 		
-		else{
+		else {
 		$(".loginBox").animate({height: '330px'});
 		//$(".inputContainer").animate({height: '100px'});
 		$(".registrationButton").animate({ 
-       	 top: "-=90px",
+       	 top: "-=140px",
       	}, '400');
-		$('#passwordConfirmationOne').remove().hide().show('normal');
-		$('#passwordConfirmationTwo').remove().hide().show('normal');
+		
+		$('#passwordConfirmation').hide('fast').remove();
+		$('#emailConfirmationOne').hide('fast').remove();
+		$('#emailConfirmationTwo').hide('fast').remove();
+		
+		//Clear out inputs
+		clearOutInputs();
 		
 		toggleHeight = true;
+		
+		//Toggling login and registration button text
+		toggleUsernameAndRegistrationButtonText();
+		
+
 		}//end - else statement
 		
 		
