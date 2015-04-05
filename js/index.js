6915e4390bd0eaaf7333a55f71d4fdc0ba2577a3
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-05 09:26:07 -0400

    Polished the DOM, now working with JQuery and Parse

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/**
+ * @author
+ */
+
+$( document ).ready(function() {
+    
+	//listening for LOGIN button to be pressed
+	$("#loginButtonPress").click(function(){
+   	
+	var username = $('#usernameInputField').val();
+	var password = $('#passwordInputField').val();
+	
+	
+
+});//end function
+
+});
