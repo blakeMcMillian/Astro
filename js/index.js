14607c1248d6aaa695b6fdc87fbc49e2fd657c51
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-05 12:37:09 -0400

    Added an registration animation

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@
  */
 
 $( document ).ready(function() {
+	
+	//Variables
+	var toggleHeight = true;
+	
+	//Setting up Parse APPID + Javascript Key
+	Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
     
 	//listening for LOGIN button to be pressed
 	$("#loginButtonPress").click(function(){
@@ -10,8 +16,63 @@ $( document ).ready(function() {
 	var username = $('#usernameInputField').val();
 	var password = $('#passwordInputField').val();
 	
+	//Parse login verification
+	Parse.User.logIn(username, password, {
+ 	 success: function(user) 
+	{
+    // Do stuff after successful login.
+	alert('worked');
+ 	 },
+  	error: function(user, error) {
+    // The login failed. Check error to see why.
+	alert('NOT');
+  }
+});//end parse login function
+	
+});//end - loginButtonPress event
+
+//listening for LOGIN button to be pressed
+	$("#registrationButtonPress").click(function(){
+
+		if(toggleHeight)
+		{
+		$(".loginBox").animate({height: '410px'});
+		//$(".inputContainer").animate({height: '190px'});
+		$(".registrationButton").animate({ 
+       	 top: "+=90px",
+      	}, '400');
+
+		//setting an element equal to the password form field
+		$('.inputContainer').append('<input type="password" value="" class=".loginBox input center-block" placeholder="Confirm Password" id="passwordConfirmationOne" />');
+		
+		$('#passwordConfirmationOne').hide().show('slow');
+		
+		$('.inputContainer').append('<input type="password" value="" class=".loginBox input center-block" placeholder="Re-Enter Password" id="passwordConfirmationTwo" />');
+		
+		$('#passwordConfirmationTwo').hide().show('slow');
+		
+		toggleHeight = false;
+		
+		}//end - if statement
+		
+		else{
+		$(".loginBox").animate({height: '330px'});
+		//$(".inputContainer").animate({height: '100px'});
+		$(".registrationButton").animate({ 
+       	 top: "-=90px",
+      	}, '400');
+		$('#passwordConfirmationOne').remove().hide().show('normal');
+		$('#passwordConfirmationTwo').remove().hide().show('normal');
+		
+		toggleHeight = true;
+		}//end - else statement
+		
+		
+	});//end - registrationButtonFunction
 	
 
-});//end function
 
-});
+
+
+
+}); 
