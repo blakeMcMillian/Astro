65547039ad098bf2262257f9fceadd45af0b9294
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-07 10:27:39 -0400

    Implemented the user registration functionlaity

--- js/index.js
+++ js/index.js
@@ -8,6 +8,8 @@ Blake McMillian
 
 //Document ready function
 $( document ).ready(function() {
+	
+	
 	 
 	//Listening for LOGIN button to be pressed
 	$("#loginButtonPress").click(function(){
@@ -15,7 +17,12 @@ $( document ).ready(function() {
 		//Check the form fields in the dom to determine if the user is registering
 		if(userIsRegistering)
 		{
+			//Resetting user registration variables
+			var userIsValid = true;
+			var passwordIsValid = false;
+			var emailIsValid = false;
 			
+			//alert("Registering new user");
 			//Getting the user's registration information
 		   	var usernameRegistration = $('#usernameInputField').val();
 		   	var passwordRegistration = $('#passwordInputField').val();
@@ -24,21 +31,63 @@ $( document ).ready(function() {
 			var emailConfirmationRegistration = $('#emailConfirmationTwo').val();
 			
 			//Make sure that the username does not exist
+			var queryUsers = new Parse.Query(Parse.User);
 			
-			//Confirm that both passwords are the same
+			queryUsers.equalTo("username", usernameRegistration); 
+			queryUsers.find({
+			  success: function(user) {
+			    // Do stuff
+				if(user.length > 0)
+				userIsValid = false;
+			  }
+			});
 			
-			//Confirm that both email addresses are the same
-				
-			//Make sure the last 4 characters of the string ends in ".com"
+			if(userIsValid === true)
+			{
+				//Confirm that both passwords are the same
+				if(passwordRegistration === passwordConfirmation)
+				passwordIsValid = true;
+			
+				//Confirm that both email addresses are the same
+				if(emailRegistration === emailConfirmationRegistration)
+				emailIsValid = true;
+					//Make sure the last 4 characters of the string ends in ".com"
+				if(passwordIsValid && emailIsValid)
+				{
+					var user = new Parse.User();
+					user.set("username", usernameRegistration);
+					user.set("password", passwordConfirmation);
+					user.set("email", emailRegistration);
+					
+					user.signUp(null, {
+					  success: function(user) {
+					    // Hooray! Let them use the app now.
+						alert("Registration Sucessful");
+					  },
+					  error: function(user, error) {
+					    // Show the error message somewhere and let the user try again.
+					    alert("Error: " + error.code + " " + error.message);
+					  }
+					});//end - userSignupBLock
+					
+				}//end - conditional
+								
+			}//end - if conditional
+			
+			
+			
 
 		}//end - if conditional
 		
-	//Obtaining the username and password from the user
-	username = $('#usernameInputField').val();
-	password = $('#passwordInputField').val();
-	
-	//Authenticating the user's credientials
-	parseLoginAuthentication(username,password);
+	if(userIsRegistering === false)
+	{
+		//Obtaining the username and password from the user
+		username = $('#usernameInputField').val();
+		password = $('#passwordInputField').val();
+		
+		//Authenticating the user's credientials
+		parseLoginAuthentication(username,password);
+	}//end conditional
 	
   });//end - loginButtonPress event
 
