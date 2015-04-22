5bbbfe3d27ee86c2d19ea557270a26d45260df18
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-22 16:14:46 -0400

    Updated the Dashbaord page with Javascript

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,10 @@
  * @author
  Developer: Blake McMillian
  */
+Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
 
 $( document ).ready(function() {
-   alert("Hello world");
+   
+	var currentUser = Parse.User.current();
+	alert(currentUser.get("username"));
 });
