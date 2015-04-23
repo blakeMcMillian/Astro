42ff2dc0a1f2a0cb8b45a62abf4e37d5ed594800
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-23 11:29:32 -0400

    Content on the dashboard now updates with the latest information

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,17 +2,44 @@
  * @author
  Developer: Blake McMillian
  */
-Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
+
 
 $( document ).ready(function() {
+
+	Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
    
+	//Setting up the parse user field
 	var currentUser = Parse.User.current();
+
+	//Refreshing the User Instance
+	currentUser.fetch({
+	  success: function(currentUser) {
+	    // The object was refreshed successfully.
+	  },
+	  error: function(currentUser, error) {
+	    // The object was not refreshed successfully.
+	    // error is a Parse.Error with an error code and message.
+	  }
+	});
 	
+	//Setting the username field
 	$("#usernameField").text(currentUser.get("username"));
 	
+	//Setting the user rank field
 	var rank = "Rank: "+currentUser.get("rank");
-	
-	$("#levelField").text(currentUser.get("level"));
 	$("#rankField").text(rank);
+
+	//Setting the level for the user
+	var level = "Level: "+currentUser.get("level");
+	$("#levelField").text(level);
+
 	
+	//Getting the html from the progress bar
+	var html = $("#progressbarField").html();
+	//Getting the progress for a given user
+	 var progress = currentUser.get("progress");
+
+	//Setting the progressbar with data from parse
+	$("#progressbarField").attr('data-pro-bar-percent', progress);
+
 });
