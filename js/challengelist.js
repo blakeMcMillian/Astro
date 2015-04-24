eeac2aeb1cb8f2f39703ac5d1adeea88aadb3a84
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-23 21:16:55 -0400

    Implemented some cashing functionality to the challenges page

---
+++ js/challengelist.js
@@ -0,0 +1,8 @@
+/**
+ * @author
+ */
+$( document ).ready(function() {
+	
+	alert( "Challenge = " + sessionStorage.getItem("challenge"));
+   
+});
--- js/challenges.js
+++ js/challenges.js
@@ -2,5 +2,50 @@
  * @author
  */
 $( document ).ready(function() {
+	
+	//Detecting page click on challenges
+	$("#criticalthinkingbutton").click(function(){
+       	//Do stuff when clicked
+		sessionStorage.setItem("challenge", "Critical Thinking");
+    	});
+	
+	//Detecting page click on challenges
+	$("#memorybutton").click(function(){
+       	//Do stuff when clicked
+		sessionStorage.setItem("challenge", "Memory");
+    	});
+
+	//Detecting page click on challenges
+	$("#recognitionbutton").click(function(){
+       	//Do stuff when clicked
+		sessionStorage.setItem("challenge", "Recognition");
+    	});
+
+	//Detecting page click on challenges
+	$("#definitionbutton").click(function(){
+       	//Do stuff when clicked
+		sessionStorage.setItem("challenge", "Definitions");
+    });
+
+    //Detecting page click on challenges
+	$("#aboveandbeyondbutton").click(function(){
+       	//Do stuff when clicked
+		sessionStorage.setItem("challenge", "Above and Above");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
    
 });
