09bf5106bce5591737744373a57529d9fc30877d
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-24 18:48:52 -0400

    Updated the questions page

--- js/models/indexModel.js
+++ js/models/indexModel.js
@@ -28,7 +28,7 @@ var userSigninFunction = function(user){
 		user.signUp(null, {
 		  success: function(user) {
 		    // Hooray! Let them use the app now.
-			alert("Registration Successful");
+			window.location.href = "dashboard.html";
 		  },
 		  error: function(user, error) {
 		    // Show the error message somewhere and let the user try again.
