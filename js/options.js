6aa8470d9d90f1231c2658cecadc920b1751fb81
blakeMcMillian <b4mcmillian@gmail.com>
2015-04-26 22:16:06 -0400

    Implemented the options page

--- js/options.js
+++ js/options.js
@@ -6,15 +6,18 @@ $( document ).ready(function() {
 	//Reinitilizng Parse
 	Parse.initialize("l3iYSEoRauE5ctDyD6CwojGCGIyJHxeCmgEMhnjZ", "prAydsNAqfn6j4BYudc9iJhvavc0C5IcMUyOC6Yj");
 	var currentUser = Parse.User.current();
+
 	//setting the inital image to the users default image
 	$("#profileImage").attr("src",currentUser.get("profilePicture").url());
 
-
 	var pagePosition = 0;
 	var profileImageArray = [];
-	
 
-	 var profilePicture = Parse.Object.extend("ProfilePictures");
+	var currentUser = Parse.User.current();
+	currentUser.fetch({
+	  success: function(currentUser) {
+	    // The object was refreshed successfully.
+	    var profilePicture = Parse.Object.extend("ProfilePictures");
 	 var query = new Parse.Query(profilePicture);
 	 query.find({
  	 success: function(results) {
@@ -28,12 +31,24 @@ $( document ).ready(function() {
 	  }
 	});
 	
+	    
+	  },
+	  error: function(currentUser, error) {
+	    // The object was not refreshed successfully.
+	    // error is a Parse.Error with an error code and message.
+	  }
+	});
+	
+
+	 
 
 	$(".buttonNext").click(function(e){
 
 	    	pagePosition++;
 	    	pagePosition = pagePosition%profileImageArray.length;
 	    	$("#profileImage").attr("src",profileImageArray[pagePosition].get('largeImage').url());
+	    	currentUser.set('profilePicture',profileImageArray[pagePosition].get('largeImage'));
+	    	currentUser.save();
 	});
 	$(".buttonPrevious").click(function(e){
 
@@ -41,56 +56,32 @@ $( document ).ready(function() {
 			pagePosition = pagePosition%profileImageArray.length;
 			pagePosition = Math.abs(pagePosition);
 			$("#profileImage").attr("src",profileImageArray[pagePosition].get('largeImage').url());
+			currentUser.set('profilePicture',profileImageArray[pagePosition].get('largeImage'));
+			currentUser.save();
+
 	});
 
 		$(".button_1").click(function(e){
 
-			userSelections[pagePosition-1] = 'one';
-			
+			var username = $('#usernameInputField').val();
+			currentUser.set('username',username);
+			currentUser.save();
 
-			jQuery('#choice1').css('opacity', '1.0');
-			jQuery('#choice2').css('opacity', '0.5');
-			jQuery('#choice3').css('opacity', '0.5');
-			jQuery('#choice4').css('opacity', '0.5');
-			
-		    
 		});
 		$(".button_2").click(function(e){
 		
-			userSelections[pagePosition-1] = 'two';
-			
+			var email = $('#emailInputField').val();
+			currentUser.set('email',email);
+			currentUser.save();
 
-			jQuery('#choice1').css('opacity', '0.5');
-			jQuery('#choice2').css('opacity', '1.0');
-			jQuery('#choice3').css('opacity', '0.5');
-			jQuery('#choice4').css('opacity', '0.5');
-
-		    
 		});
 		$(".button_3").click(function(e){
 			
-			userSelections[pagePosition-1] = 'three';
-			
-
-			jQuery('#choice1').css('opacity', '0.5');
-			jQuery('#choice2').css('opacity', '0.5');
-			jQuery('#choice3').css('opacity', '1.0');
-			jQuery('#choice4').css('opacity', '0.5');
-
-		    
+			var password = $('#passwordInputField').val();
+		    currentUser.set('password',password);
+		    currentUser.save();
 		});
-		$(".button_4").click(function(e){
-
-			userSelections[pagePosition-1] = 'four';
-			
-
-			jQuery('#choice1').css('opacity', '0.5');
-			jQuery('#choice2').css('opacity', '0.5');
-			jQuery('#choice3').css('opacity', '0.5');
-			jQuery('#choice4').css('opacity', '1.0');
-		    
-		});
-
+		
 		
 
 
